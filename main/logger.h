4037a7ddae5f359ea925405b38dc9858c1821eae
Vitalii M <r3moob@gmail.com>
2019-04-30 14:16:35 +0300

    - added logger.h

---
+++ main/logger.h
@@ -0,0 +1,16 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+// Define where debug output will be printed.
+#define D_PRINTER Serial
+
+// Setup debug printing macros.
+#ifdef _DEBUG_
+#define D_PRINT(...) { D_PRINTER.print(__VA_ARGS__); }
+#define D_PRINTLN(...) { D_PRINTER.println(__VA_ARGS__); }
+#else
+#define D_PRINT(...) {}
+#define D_PRINTLN(...) {}
+#endif
+
+#endif
